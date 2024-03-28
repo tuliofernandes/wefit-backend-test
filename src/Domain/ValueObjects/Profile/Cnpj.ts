20500aa2407c5135fc9b56996916fa5bcbbfdddf
Túlio Fernandes <tuliomf09@gmail.com>
2024-03-28 16:43:03 -0300

    feat: add Cnpj value object

---
+++ src/Domain/ValueObjects/Profile/Cnpj.ts
@@ -0,0 +1,14 @@
+export class Cnpj {
+  constructor(private readonly value: string) {
+    if (!this.isValid(value)) throw new Error("Invalid CNPJ");
+  }
+
+  private isValid(value: string): boolean {
+    if (!/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/.test(value)) return false;
+    return true;
+  }
+
+  toString(): string {
+    return this.value;
+  }
+}
