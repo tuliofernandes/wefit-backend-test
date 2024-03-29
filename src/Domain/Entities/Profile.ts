6fd14cc679cd67e523fb2fdf6ad23bd1d4714a43
Túlio Fernandes <tuliomf09@gmail.com>
2024-03-29 08:52:47 -0300

    feat: ensure toJson method returns the json representation of Profile

--- src/Domain/Entities/Profile.ts
+++ src/Domain/Entities/Profile.ts
@@ -15,6 +15,23 @@ import {
   Uf,
 } from "@/Domain/ValueObjects/Profile";
 
+export type ProfileSchema = {
+  type: string;
+  cpf: string;
+  cnpj?: string;
+  name: string;
+  cellPhone: string;
+  phone: string;
+  email: string;
+  cep: string;
+  street: string;
+  addressNumber: string;
+  addressComplement: string;
+  neighborhood: string;
+  city: string;
+  uf: string;
+};
+
 export class Profile {
   constructor(
     private readonly type: ProfileType,
@@ -34,5 +51,24 @@ export class Profile {
   ) {
     if (this.type === ProfileType.PJ && !this.cnpj)
       throw new DomainException("CNPJ is required for PJ profiles");
+  }
+
+  public toJson(): ProfileSchema {
+    return {
+      type: this.type.toString(),
+      cpf: this.cpf.toString(),
+      cnpj: this.cnpj?.toString(),
+      name: this.name.toString(),
+      cellPhone: this.cellPhone.toString(),
+      phone: this.phone.toString(),
+      email: this.email.toString(),
+      cep: this.cep.toString(),
+      street: this.street.toString(),
+      addressNumber: this.addressNumber.toString(),
+      addressComplement: this.addressComplement.toString(),
+      neighborhood: this.neighborhood.toString(),
+      city: this.city.toString(),
+      uf: this.uf.toString(),
+    };
   }
 }
