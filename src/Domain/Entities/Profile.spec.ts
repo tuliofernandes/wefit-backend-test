a1d9cb56ab31a2693a64c66c51ba85539c8adc6b
Túlio Fernandes <tuliomf09@gmail.com>
2024-03-29 08:52:57 -0300

    test: ensure toJson method returns the json representation of Profile

--- src/Domain/Entities/Profile.spec.ts
+++ src/Domain/Entities/Profile.spec.ts
@@ -3,6 +3,7 @@ import {
   AddressNumber,
   Cep,
   City,
+  Cnpj,
   Cpf,
   Email,
   FullName,
@@ -26,14 +27,30 @@ describe("[Entity]: Profile", () => {
     email: "department@company.com",
     cep: "18570-000",
     street: "Rua Afonso Athanázio",
-    addressNumber: 270,
+    addressNumber: "270",
     addressComplement: "Bl 5 apto 82",
     neighborhood: "Jd. Letícia",
     city: "Conchas",
     uf: "SP",
   };
+  const profileFixture = new Profile(
+    ProfileType.PJ,
+    new Cpf(profileFixtureJson.cpf),
+    new Cnpj(profileFixtureJson.cnpj),
+    new FullName(profileFixtureJson.name),
+    new Phone(profileFixtureJson.cellPhone),
+    new Phone(profileFixtureJson.phone),
+    new Email(profileFixtureJson.email),
+    new Cep(profileFixtureJson.cep),
+    new Street(profileFixtureJson.street),
+    new AddressNumber(profileFixtureJson.addressNumber),
+    new AddressComplement(profileFixtureJson.addressComplement),
+    new Neighborhood(profileFixtureJson.neighborhood),
+    new City(profileFixtureJson.city),
+    new Uf(profileFixtureJson.uf)
+  );
 
-  describe("methods", () => {
+  describe("validation", () => {
     it("Should throw DomainException if ProfileType is PJ but no CNPJ is provided", () => {
       expect(
         () =>
@@ -54,6 +71,12 @@ describe("[Entity]: Profile", () => {
             new Uf(profileFixtureJson.uf)
           )
       ).toThrow(new DomainException("CNPJ is required for PJ profiles"));
+    });
+  });
+
+  describe("methods", () => {
+    it("toJson should return the JSON representation of the entity", () => {
+      expect(profileFixture.toJson()).toEqual(profileFixtureJson);
     });
   });
 });
