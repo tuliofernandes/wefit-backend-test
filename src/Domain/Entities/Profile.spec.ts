e111ad2d0c15a1202db8713a6f86a5b8589bff1a
Túlio Fernandes <tuliomf09@gmail.com>
2024-04-01 22:56:20 -0300

    test: ensure Profile throws DomainException if type is PF but CNPJ is provided

--- src/Domain/Entities/Profile.spec.ts
+++ src/Domain/Entities/Profile.spec.ts
@@ -17,67 +17,61 @@ import { Profile } from "./Profile";
 import { ProfileType } from "@/Domain/Enums/ProfileType";
 import { DomainException } from "@/Domain/Exceptions/DomainException";
 
-describe("[Entity]: Profile", () => {
-  const profileFixtureJson = {
-    type: ProfileType.PJ,
-    cpf: "123.456.789-00",
-    cnpj: "12.345.678/0001-90",
-    name: "Empresa Teste",
-    cellPhone: "(14) 99999-9999",
-    phone: "(14) 9999-9999",
-    email: "department@company.com",
-    cep: "18570-000",
-    street: "Rua Afonso Athanázio",
-    addressNumber: "270",
-    addressComplement: "Bl 5 apto 82",
-    neighborhood: "Jd. Letícia",
-    city: "Conchas",
-    uf: "SP",
-  };
-  const profileFixture = new Profile(
-    new Type(ProfileType.PJ),
-    new Cpf(profileFixtureJson.cpf),
-    new Cnpj(profileFixtureJson.cnpj),
-    new FullName(profileFixtureJson.name),
-    new Phone(profileFixtureJson.cellPhone),
-    new Phone(profileFixtureJson.phone),
-    new Email(profileFixtureJson.email),
-    new Cep(profileFixtureJson.cep),
-    new Street(profileFixtureJson.street),
-    new AddressNumber(profileFixtureJson.addressNumber),
-    new AddressComplement(profileFixtureJson.addressComplement),
-    new Neighborhood(profileFixtureJson.neighborhood),
-    new City(profileFixtureJson.city),
-    new Uf(profileFixtureJson.uf)
-  );
+import {
+  profileFixtureEntity,
+  profileFixtureSchema,
+} from "../../../tests/fixtures/Entities/Profile";
 
+describe("[Entity]: Profile", () => {
   describe("validation", () => {
     it("Should throw DomainException if ProfileType is PJ but no CNPJ is provided", () => {
       expect(
         () =>
           new Profile(
             new Type(ProfileType.PJ),
-            new Cpf(profileFixtureJson.cpf),
+            new Cpf(profileFixtureSchema.cpf),
             null,
-            new FullName(profileFixtureJson.name),
-            new Phone(profileFixtureJson.cellPhone),
-            new Phone(profileFixtureJson.phone),
-            new Email(profileFixtureJson.email),
-            new Cep(profileFixtureJson.cep),
-            new Street(profileFixtureJson.street),
-            new AddressNumber(profileFixtureJson.addressNumber),
-            new AddressComplement(profileFixtureJson.addressComplement),
-            new Neighborhood(profileFixtureJson.neighborhood),
-            new City(profileFixtureJson.city),
-            new Uf(profileFixtureJson.uf)
+            new FullName(profileFixtureSchema.name),
+            new Phone(profileFixtureSchema.cellPhone),
+            new Phone(profileFixtureSchema.phone),
+            new Email(profileFixtureSchema.email),
+            new Cep(profileFixtureSchema.cep),
+            new Street(profileFixtureSchema.street),
+            new AddressNumber(profileFixtureSchema.addressNumber),
+            new AddressComplement(profileFixtureSchema.addressComplement),
+            new Neighborhood(profileFixtureSchema.neighborhood),
+            new City(profileFixtureSchema.city),
+            new Uf(profileFixtureSchema.uf)
           )
       ).toThrow(new DomainException("CNPJ is required for PJ profiles"));
+    });
+
+    it("Should throw DomainException if ProfileType is PF but CNPJ is provided", () => {
+      expect(
+        () =>
+          new Profile(
+            new Type(ProfileType.PF),
+            new Cpf(profileFixtureSchema.cpf),
+            new Cnpj(profileFixtureSchema.cnpj),
+            new FullName(profileFixtureSchema.name),
+            new Phone(profileFixtureSchema.cellPhone),
+            new Phone(profileFixtureSchema.phone),
+            new Email(profileFixtureSchema.email),
+            new Cep(profileFixtureSchema.cep),
+            new Street(profileFixtureSchema.street),
+            new AddressNumber(profileFixtureSchema.addressNumber),
+            new AddressComplement(profileFixtureSchema.addressComplement),
+            new Neighborhood(profileFixtureSchema.neighborhood),
+            new City(profileFixtureSchema.city),
+            new Uf(profileFixtureSchema.uf)
+          )
+      ).toThrow(new DomainException("CNPJ is not required for PF profiles"));
     });
   });
 
   describe("methods", () => {
     it("toJson should return the JSON representation of the entity", () => {
-      expect(profileFixture.toJson()).toEqual(profileFixtureJson);
+      expect(profileFixtureEntity.toJson()).toEqual(profileFixtureSchema);
     });
   });
 });
