616b2b537e8da8475f37ee7f1d1d70e0a00082e6
Túlio Fernandes <tuliomf09@gmail.com>
2024-04-02 15:31:12 -0300

    refactor: remove hardcoded sensitive information from EnvAdapter

--- src/Infra/Configs/env.ts
+++ src/Infra/Configs/env.ts
@@ -1,15 +1,15 @@
 export class EnvAdapter {
   static http = {
-    listenPort: Number(process.env.HTTP_PORT || 4568),
+    listenPort: Number(process.env.HTTP_PORT) ?? 4568,
   };
 
   static readonly databaseSettings = {
     authentication: {
-      host: process.env.MYSQLDB_HOST ?? "localhost",
+      host: process.env.MYSQLDB_HOST ?? "",
       port: Number(process.env.MYSQLDB_PORT) ?? 3306,
-      username: process.env.MYSQL_USER ?? "root",
-      password: process.env.MYSQLDB_PASSWORD ?? "senha_root_123",
-      database: process.env.MYSQLDB_DATABASE ?? "wefit",
+      username: process.env.MYSQL_USER ?? "",
+      password: process.env.MYSQLDB_PASSWORD ?? "",
+      database: process.env.MYSQLDB_DATABASE ?? "",
     },
   };
 }
