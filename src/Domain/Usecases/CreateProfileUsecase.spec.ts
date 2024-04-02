711bf21b8c402956a6385790e1eb4c0c3e91e9cf
Túlio Fernandes <tuliomf09@gmail.com>
2024-04-02 00:37:28 -0300

    test: ensure CreateProfileUsecase is able to throw an error if user with the same CPF already exists

--- src/Domain/Usecases/CreateProfileUsecase.spec.ts
+++ src/Domain/Usecases/CreateProfileUsecase.spec.ts
@@ -19,4 +19,17 @@ describe("[Usecase] CreateProfileUsecase", () => {
       new DomainException("Profile with the same email already exists")
     );
   });
+
+  it("Should throw an error if user with the same CPF already exists", () => {
+    jest.spyOn(profileRepository, "findByEmail").mockResolvedValueOnce(null);
+    jest
+      .spyOn(profileRepository, "findByCpf")
+      .mockResolvedValueOnce(profileFixtureEntity);
+    const sut = new CreateProfileUsecase(profileRepository);
+    const createPromise = sut.execute(profileFixtureEntity);
+
+    expect(createPromise).rejects.toThrow(
+      new DomainException("Profile with the same CPF already exists")
+    );
+  });
 });
