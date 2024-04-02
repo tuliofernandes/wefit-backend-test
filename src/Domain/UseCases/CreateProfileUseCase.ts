58fb2b0e51a01b6da47f1208d585ad89112a7435
Túlio Fernandes <tuliomf09@gmail.com>
2024-04-01 23:52:31 -0300

    test: ensure CreateProfileUsecase throws an error if user already exists

--- src/Domain/UseCases/CreateProfileUseCase.ts
+++
@@ -1,8 +0,0 @@
-import { Profile } from "@/Domain/Entities";
-import { IProfileRepository } from "@/Domain/Protocols";
-
-export class CreateProfileUseCase {
-  constructor(private readonly repository: IProfileRepository) {}
-
-  async execute(profile: Profile) {}
-}
---
+++ src/Domain/Usecases/CreateProfileUsecase.spec.ts
@@ -0,0 +1,23 @@
+import { DomainException } from "@/Domain/Exceptions/DomainException";
+import { CreateProfileUsecase } from "@/Domain/Usecases/CreateProfileUsecase";
+
+import { ProfileRepository } from "@/Infra/Repositories/ProfileRepository";
+
+import { profileFixtureEntity } from "../../../tests/fixtures/Entities/Profile";
+
+describe("[Usecase] CreateProfileUsecase", () => {
+  const profileRepository = new ProfileRepository();
+
+  it("Should throw an error if user already exists", () => {
+    jest
+      .spyOn(profileRepository, "findByEmail")
+      .mockResolvedValueOnce(profileFixtureEntity);
+
+    const sut = new CreateProfileUsecase(profileRepository);
+    const createPromise = sut.execute(profileFixtureEntity);
+
+    expect(createPromise).rejects.toThrow(
+      new DomainException("Profile already exists")
+    );
+  });
+});
