99b8a535b756244441a7481f38830dd44810e911
Túlio Fernandes <tuliomf09@gmail.com>
2024-04-02 00:38:23 -0300

    test: ensure CreateProfileUsecase is able to throw an error if user with the same CNPJ already exists

--- src/Domain/Usecases/CreateProfileUsecase.spec.ts
+++ src/Domain/Usecases/CreateProfileUsecase.spec.ts
@@ -32,4 +32,18 @@ describe("[Usecase] CreateProfileUsecase", () => {
       new DomainException("Profile with the same CPF already exists")
     );
   });
+
+  it("Should throw an error if user with the same CNPJ already exists", () => {
+    jest.spyOn(profileRepository, "findByEmail").mockResolvedValueOnce(null);
+    jest.spyOn(profileRepository, "findByCpf").mockResolvedValueOnce(null);
+    jest
+      .spyOn(profileRepository, "findByCnpj")
+      .mockResolvedValueOnce(profileFixtureEntity);
+    const sut = new CreateProfileUsecase(profileRepository);
+    const createPromise = sut.execute(profileFixtureEntity);
+
+    expect(createPromise).rejects.toThrow(
+      new DomainException("Profile with the same CNPJ already exists")
+    );
+  });
 });
--- src/Domain/Usecases/CreateProfileUsecase.ts
+++ src/Domain/Usecases/CreateProfileUsecase.ts
@@ -1,13 +1,12 @@
 import { Profile } from "@/Domain/Entities";
 import { IProfileRepository } from "@/Domain/Protocols";
 import { DomainException } from "@/Domain/Exceptions/DomainException";
-import { ProfileType } from "@/Domain/Enums/ProfileType";
 
 export class CreateProfileUsecase {
   constructor(private readonly profileRepository: IProfileRepository) {}
 
   async execute(profile: Profile) {
-    const profileIsPj = profile.getType().toString() === ProfileType.PJ;
+    const profileIsPj = profile.getCnpj() !== null;
     const [foundByEmail, foundByCpf, foundByCnpj] = await Promise.all([
       this.profileRepository.findByEmail(profile.getEmail()),
       this.profileRepository.findByCpf(profile.getCpf()),
